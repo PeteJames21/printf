a8c45039f5b72408c0e9bb44f07c423526f75629
kelvin Gitonga <100723539+Gikaria@users.noreply.github.com>
2023-04-25 22:01:02 +0300

    Update test_1.c

--- tests/test_1.c
+++ tests/test_1.c
@@ -11,11 +11,11 @@ int main(void)
 	int n;
 	/* print string with no specifier*/
 	n = _printf("I am inevitable");
-	printf(" (String len %d)\n", n);
+	printf(" (string len %d)\n", n);
 
 	/*print a string specifiers*/
 	n = _printf("The collaborators for this project are; %s,and %s \n", "Peter", "Kelvin");
-	printf("(String len %d)\n", n);
+	printf("(string len %d)\n", n);
 
 	/* Print string with invalid specifier */
 	n = _printf("AI is %c\n", "good");
